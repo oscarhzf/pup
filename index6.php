8b2c05ee98390c7e07d0eadf8b82c70edf798446
Ziyuan-Cheng <ziyuancheng35@gmail.com>
2021-03-16 15:14:13 +0800

    index 6

--- index6.php
+++ index6.php
@@ -283,28 +283,8 @@
 				},
 			})
         </script>
-        <script>
-                                function initMap() {
-                                    const map = new google.maps.Map(document.getElementById("map"), {
-                                      zoom: 3,
-                                      center: { lat: 0, lng: -180 },
-                                      mapTypeId: "terrain",
-                                    });
-                                    const flightPlanCoordinates = [
-                                      { lat: 37.7722, lng: -122.2144 },
-                                      { lat: 21.291, lng: -157.821 },
-                                      { lat: -18.142, lng: 178.431 },
-                                      { lat: -27.467, lng: 153.027 },
-                                    ];
-                                    const flightPath = new google.maps.Polyline({
-                                      path: flightPlanCoordinates,
-                                      geodesic: true,
-                                      strokeColor: "#FF0000",
-                                      strokeOpacity: 1.0,
-                                      strokeWeight: 2,
-                                    });
-                                    flightPath.setMap(map);
-                                  }
+        <script src="map.js">
+                                
         </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDF8zxQcSyJHyurdoy4Ef02tMPw7RmvwM4&callback=initMap&libraries=&v=weekly" async></script>
     </body>
